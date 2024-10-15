0ab207968b11dc332e2942abc2069bac22a03c91
ivanurbanodev <ivan.urbano.n@uni.pe>
2024-10-15 10:18:49 -0500

    Se mejoró la gestión de recursos y la economia del juego

--- app/src/infraestructuras.py
+++ app/src/infraestructuras.py
@@ -1,5 +1,25 @@
+# Definición de las infraestructuras disponibles en el juego
+# Cada infraestructura tiene un costo en oro y madera, y ofrece ciertos beneficios
 infraestructuras = {
-    "Granero": {"oro": 20, "madera": 30, "aumento_produccion": 10, "aumento_estabilidad": 5},
-    "Muralla": {"oro": 50, "madera": 50, "aumento_produccion": 0, "aumento_estabilidad": 15},
-    "Torre de vigilancia": {"oro": 30, "madera": 40, "aumento_produccion": 0, "aumento_estabilidad": 10},
+    "Granero": {
+        "oro": 20,  # Costo en oro para construir el granero
+        "madera": 30,  # Costo en madera para construir el granero
+        "aumento_produccion": 10,  # Aumenta la producción del reino en 10 unidades
+        "aumento_estabilidad": 5,  # Aumenta la estabilidad del reino en 5 puntos
+        "aumento_territorio": 2  # Aumenta el número de territorios del reino en 2
+    },
+    "Muralla": {
+        "oro": 50,  # Costo en oro para construir la muralla
+        "madera": 50,  # Costo en madera para construir la muralla
+        "aumento_produccion": 0,  # No afecta la producción del reino
+        "aumento_estabilidad": 15,  # Aumenta la estabilidad del reino en 15 puntos
+        "aumento_territorio": 5  # Aumenta el número de territorios del reino en 5
+    },
+    "Torre de vigilancia": {
+        "oro": 30,  # Costo en oro para construir la torre de vigilancia
+        "madera": 40,  # Costo en madera para construir la torre de vigilancia
+        "aumento_produccion": 0,  # No afecta la producción del reino
+        "aumento_estabilidad": 10,  # Aumenta la estabilidad del reino en 10 puntos
+        "aumento_territorio": 3  # Aumenta el número de territorios del reino en 3
+    }
 }
