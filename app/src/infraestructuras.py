84ff7489f991087190f3541bdb3f5d663f540be0
ArbuesEnriquePerezVillegas <arbues.perez.v@uni.pe>
2024-10-14 22:59:57 -0500

    Se agrego la nueva estructura principal del trabajo

--- app/src/infraestructuras.py
+++
@@ -1,5 +0,0 @@
-infraestructuras = {
-    "Granero": {"oro": 20, "madera": 30, "aumento_produccion": 10, "aumento_estabilidad": 5},
-    "Muralla": {"oro": 50, "madera": 50, "aumento_produccion": 0, "aumento_estabilidad": 15},
-    "Torre de vigilancia": {"oro": 30, "madera": 40, "aumento_produccion": 0, "aumento_estabilidad": 10},
-}
