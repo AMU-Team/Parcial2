94e05f38f61fbb130555d45d5c75310e3739b445
ivanurbanodev <ivan.urbano.n@uni.pe>
2024-10-15 23:49:22 -0500

    Se configuro grafana y prometheus

--- Kingdom_Conquest/server/src/main.py
+++ Kingdom_Conquest/server/src/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from src.database_connection import init_db
 from src.routes import router as reino_router  # Importa el router desde routes.py
+from prometheus_client import generate_latest, CONTENT_TYPE_LATEST  # Importar funciones para métricas de Prometheus
 
 app = FastAPI()
 
@@ -16,3 +17,11 @@ app.include_router(reino_router, prefix="/api/v1")  # Prefijo opcional para las 
 @app.get("/")
 def read_root():
     return {"message": "Bienvenido a Kingdom Conquest API"}
+
+@app.get("/metrics")
+def metrics():
+    """
+    Endpoint para obtener métricas de Prometheus.
+    Retorna las métricas en el formato adecuado.
+    """
+    return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
